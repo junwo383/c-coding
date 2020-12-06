ab0f97e80e4f158a2d00bfb16e21fc2beac76f06
junwo383 <75435635+junwo383@users.noreply.github.com>
2020-12-06 13:52:38 +0900

    Add files via upload

---
+++ 12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int num=9, num1=1;
+	printf("계산기를 만들어봅시다.!\n");
+	printf("%d+%d=%d \n", num, num1, num + num1);
+	printf("%d-%d=%d \n", num, num1, num -num1);
+	printf("%d*%d=%d \n", num, num1, num * num1);
+	printf("%d/%d=%d \n", num, num1, num / num1);
+	return 0;
+	
+}
